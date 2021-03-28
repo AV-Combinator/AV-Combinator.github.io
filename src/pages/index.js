cbbca86aac5eab7a63b21f0fbc09c7dcd981fac5
Angel Pan <angelpan@stanford.com>
2021-03-28 09:21:15 +0800

    add initial backgroudn layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,21 @@
 import React from "react"
+import * as indexStyles from '../styles/index.module.css'
+import vanishingPoint from '../images/vanishing-point.svg'
+import flames from '../images/flames.svg'
+import curve from '../images/curve.svg'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className={indexStyles.container}>
+    <SectionImage src={vanishingPoint} alt='many lines moving towards vanishing point'/>
+    <SectionImage src={flames} alt='lines curved like untame flames' />
+    <SectionImage src={curve} alt='curve in on the roadside' />
+    </div>
+  );
+}
+
+const SectionImage = ({src, alt}) => {
+  return (
+    <img className={indexStyles.sectionImage} src={src} alt={alt}/>
+  );
 }
