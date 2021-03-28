c0e5d0fec775a66c4183f150f6fe989b9a3392a7
Angel Pan <angelpan@stanford.com>
2021-03-28 10:06:36 +0800

    implement static navbar with harcoded width

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@ import * as indexStyles from '../styles/index.module.css'
 import vanishingPoint from '../images/vanishing-point.svg'
 import flames from '../images/flames.svg'
 import curve from '../images/curve.svg'
+import logo from '../images/logo.svg'
 
 export default function Home() {
   return (
     <div className={indexStyles.container}>
-    <SectionImage src={vanishingPoint} alt='many lines moving towards vanishing point'/>
-    <SectionImage src={flames} alt='lines curved like untame flames' />
-    <SectionImage src={curve} alt='curve in on the roadside' />
+      <TopBar />
+      <SectionImage src={vanishingPoint} alt='many lines moving towards vanishing point'/>
+      <SectionImage src={flames} alt='lines curved like untame flames' />
+      <SectionImage src={curve} alt='curve in on the roadside' />
     </div>
   );
 }
@@ -17,5 +19,25 @@ export default function Home() {
 const SectionImage = ({src, alt}) => {
   return (
     <img className={indexStyles.sectionImage} src={src} alt={alt}/>
+  );
+}
+
+const TopBar = () => {
+  return (
+    <div className={indexStyles.topBar}>
+      <img src={logo} alt='AV Combinator logo'/>
+      <NavBar />
+    </div>
+  );
+}
+
+const NavBar = () => {
+  return (
+  <div className={indexStyles.navBar}>
+    <span className={indexStyles.navText}>Home</span>
+    <span className={indexStyles.navText}>About Us</span>
+    <span className={indexStyles.navText}>Our Team</span>
+    <span className={indexStyles.navText}>Contact Us</span>
+  </div>
   );
 }
