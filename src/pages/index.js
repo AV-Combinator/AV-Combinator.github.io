4c61d08c9c240f1172c20699ba7fee160d8d57e9
Angel Pan <angelpan@stanford.com>
2021-03-29 23:41:49 +0800

    fix background image; add navbar functionality; log button click event

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,62 +8,72 @@ import niehaus from '../images/niehaus.jpeg'
 import lawrence from '../images/lawrence.jpeg'
 import linkedin from '../images/linkedin.svg'
 import email from '../images/email.svg'
+import { Link, animateScroll as scroll } from "react-scroll";
 
 export default function Home() {
   return (
-    <div className={indexStyles.container}>
-      <div>
-        <TopBar />
-        <Header>A New Approach to Autonomous Vehicle Safety</Header>
-        <Button padding='small'>Read Our White Paper</Button>
-      </div>
+    <div>
+      <TopBar />
 
-      <div style={{margin: '400px'}}></div>
-
-      <div>
-        <Header>About Us</Header>
-        <div className={indexStyles.bodyText}>
-          AV Combinator is a safety advisory group that aims to revolutionize the way technologists, policymakers, and consumers think about autonomous vehicle safety. <br /> <br />
-          We believe what is needed, yet missing from the current autonomous vehicle ecosystem, is an approach to safety that is both feasible and comprehensive. <br /> <br />
-          To address this gap, we have put forth the <em className={indexStyles.link}>Autonomous Vehicle Modular Safety Suite White Paper</em>. <br /> <br /> <br />
-          View a summary of our white paper <span className={indexStyles.link}>here</span>. 
+      <div className={indexStyles.container}>
+        <div id='home' >
+          <Header>A New Approach to Autonomous Vehicle Safety</Header>
+          <Button 
+            padding='small'
+            onClick={() => console.log('read paper')}>
+              Read Our White Paper
+          </Button>
+          <div style={{margin: '50rem'}}></div>
         </div>
-      </div>
 
-      <div style={{margin: '200px'}}></div>
-
-      <div>
-        <Header>Our Team</Header>
-        <div className={indexStyles.memberCardList}>
-          <MemberCard name='Michael Nehmad' img={nehmad} alt='Michael Nehmad in a suit'/>
-          <MemberCard name='Richard Sun' img={sun} alt='Richard Sun smiling'/>
-          <MemberCard name='Jenn Hu' img={hu} alt='Jenn Hu in a white top'/>
-          <MemberCard name='Julius Niehaus' img={niehaus} alt='Julius Niehaus wearing nice clothes'/>
-          <MemberCard name='Troy Lawrence' img={lawrence} alt='Trow Lawrence in a white polo'/>
-        </div>
-      </div>
-
-      <div style={{margin: '130px'}}></div>
-
-      <div>
-        <Header>Contact Us</Header>
-        <div style={{display: 'flex', flexDirection: 'row'}}>
+        <div id='about-us'>
+          <Header>About Us</Header>
           <div className={indexStyles.bodyText}>
-            Submit your queries here and we will get back to you as soon as possible. 
+            AV Combinator is a safety advisory group that aims to revolutionize the way technologists, policymakers, and consumers think about autonomous vehicle safety. <br /> <br />
+            We believe what is needed, yet missing from the current autonomous vehicle ecosystem, is an approach to safety that is both feasible and comprehensive. <br /> <br />
+            To address this gap, we have put forth the <em className={indexStyles.link}>Autonomous Vehicle Modular Safety Suite White Paper</em>. <br /> <br /> <br />
+            View a summary of our white paper <span className={indexStyles.link}>here</span>. 
           </div>
-          <div>
-            <div style={{display: 'flex', flexDirection: 'row', width: '30rem'}}>
-              <FormField name='First Name'/>
-              <FormField name='Last Name' />
+          <div style={{margin: '30rem'}}></div>
+        </div>
+
+        <div id='our-team'>
+          <Header>Our Team</Header>
+          <div className={indexStyles.memberCardList}>
+            <MemberCard name='Michael Nehmad' img={nehmad} alt='Michael Nehmad in a suit'/>
+            <MemberCard name='Richard Sun' img={sun} alt='Richard Sun smiling'/>
+            <MemberCard name='Jenn Hu' img={hu} alt='Jenn Hu in a white top'/>
+            <MemberCard name='Julius Niehaus' img={niehaus} alt='Julius Niehaus wearing nice clothes'/>
+            <MemberCard name='Troy Lawrence' img={lawrence} alt='Trow Lawrence in a white polo'/>
+          </div>
+          <div style={{margin: '16rem'}}></div>
+        </div>
+
+        <div id='contact-us'>
+          <Header>Contact Us</Header>
+          <div className={indexStyles.contactSection}>
+            <div className={indexStyles.contactText}>
+              Submit your queries here and we will get back to you as soon as possible. 
             </div>
-            <FormField name='Email' />
-            <FormField name='Message' type='long'/>
-            <Button padding={'med'}>Submit</Button>
+            <div className={indexStyles.form}>
+              <div className={indexStyles.row}>
+                <FormField name='First Name'/>
+                <FormField name='Last Name' />
+              </div>
+              <div className={indexStyles.row}>
+                <FormField name='Email' />
+              </div>
+              <FormField name='Message' type='long'/>
+              <Button 
+                padding={'med'} 
+                onClick={() => console.log('submit')}>
+                  Submit
+              </Button>
+            </div>
           </div>
         </div>
+
       </div>
-
-
     </div>
   );
 }
@@ -77,14 +87,29 @@ const TopBar = () => {
   );
 }
 
+const NavBarItem = ({name, id}) => {
+  return (
+    <Link
+      className={indexStyles.navText}
+      activeClass={`${indexStyles.navText} ${indexStyles.link}`}
+      to={id}
+      spy={true}
+      smooth={true}
+      offset={-125}
+      duration={500}>
+        {name}
+    </Link>
+  );
+}
+
 const NavBar = () => {
   return (
-  <div className={indexStyles.navBar}>
-    <span className={indexStyles.navText}>Home</span>
-    <span className={indexStyles.navText}>About Us</span>
-    <span className={indexStyles.navText}>Our Team</span>
-    <span className={indexStyles.navText}>Contact Us</span>
-  </div>
+    <ul className={indexStyles.navBar}>
+      <li><NavBarItem id='home' name='Home'/></li>
+      <li><NavBarItem id='about-us' name='About Us'/></li>
+      <li><NavBarItem id='our-team' name='Our Team'/></li>
+      <li><NavBarItem id='contact-us' name='Contact Us'/></li>
+    </ul>
   );
 }
 
@@ -96,11 +121,13 @@ const Header = ({children}) => {
   );
 }
 
-const Button = ({padding, children}) => {
+const Button = ({padding, onClick, children}) => {
   const buttonPadding = padding === 'small' ? indexStyles.padS : indexStyles.padMed;
   return (
-    <div className={`${indexStyles.button} ${buttonPadding}`}>
-      <span className={indexStyles.buttonText}>{children}</span>
+    <div 
+      className={`${indexStyles.button} ${buttonPadding}`}
+      onClick={onClick}>
+        <span className={indexStyles.buttonText}>{children}</span>
     </div>
   );
 }
@@ -122,10 +149,15 @@ const MemberCard = ({name, text, img, alt}) => {
 }
 
 const FormField = ({name, type}) => {
-  const longResponse = type === 'long' ? indexStyles.longResponse : '';
   return (
-    <div className={`${indexStyles.formField} ${longResponse}`}>
-      <span className={indexStyles.formText}>{name}</span>
-    </div>
+    type === 'long' ?
+    <textarea 
+      className={`${indexStyles.formField} ${indexStyles.formText}`} 
+      placeholder={name}
+    /> :
+    <input 
+      className={`${indexStyles.formField} ${indexStyles.formText}`} 
+      type='text' 
+      placeholder={name}/>  
   )
 }
